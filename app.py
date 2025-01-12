981f19c07775de19314c78e1e562a739d3b63e56
Thara Tunthanathip <167670427+thara7640@users.noreply.github.com>
2025-01-12 09:54:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 def main():
     image_ban = Image.open('images/image1.png')
-    st.image(image_ban, use_column_width=False)
+    st.image(image_ban, use_container_width=False)
 
 
 if __name__ == '__main__':
@@ -88,7 +88,7 @@ uploaded_image = st.file_uploader("Upload a CT scan image", type=["jpg", "png", 
 if uploaded_image is not None:
     # Display the uploaded image
     image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Make prediction when the image is uploaded
     prediction = predict_icp(image)
